796aeae8afbfd1cae1841ee8849fec8dbfe80e1e
DreitonWashington <dreiton.washington@gmail.com>
2024-01-04 17:55:23 -0300

    Updated

--- src/domain/event/customer/handler/enviaConsoleLog1Handler.ts
+++
@@ -1,9 +0,0 @@
-import EventInterface from "../../@shared/event.interface";
-import EventHandlerInterface from "../../@shared/eventHandler.interface";
-
-export default class EnviaConsoleLog1Handler implements EventHandlerInterface {
-  
-  handle(event: EventInterface): void {
-    console.log("Esse é o primeiro console.log do evento: CustomerCreated");
-  }
-}
--- src/domain/repository/orderRepository.interface.ts
+++
@@ -1,4 +0,0 @@
-import Order from "../entity/order";
-import RepositoryInterface from "./repositoryInterface";
-
-export default interface OrderRepositoryInterface extends RepositoryInterface<Order> {}
